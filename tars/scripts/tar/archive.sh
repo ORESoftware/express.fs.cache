83975325c1e167eb7cd659d943bd1bf47344c14a
Olegzandr VD <alex@oresoftware.com>
2018-05-17 08:11:18 -0700

    pdev:'set'

--- tars/scripts/tar/archive.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-tar --exclude='./node_modules' \
-    --exclude='./.git' \
-    --exclude='./.idea' \
-    -cvf - . | gzip > archive.tar.gz
-
-#tar -zcf foo.tgz -s /^"$(pwd)"/"$(pwd)/package"/ "$(pwd)"
--- tars/test/fixtures/fizzbizz.js
+++
@@ -1,3 +0,0 @@
-
-
-console.log('this is the fizzbizz file.');
